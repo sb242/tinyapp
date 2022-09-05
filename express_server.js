be6687292d61636ed7f0b5b2e67e6f8389e798da
Spencer <spencerb242@proton.me>
2022-09-05 13:54:05 -0600

    added views/urls_new.ejs for new submitting new urls. Modified express_server.js to render the new urls_new.ejs. Also added express middleware to allow for reading of submitted data from urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -4,6 +4,13 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true })); //middleware used for translating sent data into readable code, from a buffer(not human readable) to string
+
+generateRandomString = () => {
+  const randomString = (Math.random() + 1).toString(36).substring(7);
+  return randomString;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,9 +29,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
