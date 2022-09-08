6f2a2e80e1e8ab8e295147754f7d79c8359c5ca9
Spencer <spencerb242@proton.me>
2022-09-07 21:53:53 -0600

    restructed urlDatabase to make newly created tinyURLS linked to the user that created it. Also modified the application so that users can only see their shortened URLS as well as modify and delete them.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const morgan = require("morgan");
 const express = require("express");
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -11,72 +12,200 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); //middleware used for translating sent data into readable code, from a buffer(not human readable) to string
 
-generateRandomString = () => {
+const generateRandomString = () => {
   const randomString = (Math.random() + 1).toString(36).substring(6);
   return randomString;
 }
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+const urlsForUser = (id) => {
+  const userURLS = {};
+  for(let element in urlDatabase) {
+    if(urlDatabase[element].userID === id) {
+      userURLS[element] = urlDatabase[element];
+    }
+  }
+  return userURLS;
+}
+
+const urlDatabase = {};
+const users = {};
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  if(req.cookies["userID"]) {
+    res.redirect("/urls");
+    return;
+  }
+  res.redirect("/login");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const userID = req.cookies["userID"];
+  const userURLS = urlsForUser(userID);
+  
+  if(!userID) {
+    res.redirect("/login");
+  }
+
+  console.log("userURLS", userURLS);
+  const templateVars = { urls: userURLS, users: users, user: userID };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  if(!req.cookies["userID"]) {
+    res.redirect("/login");
+    return;
+  }
+  const templateVars = { users: users, user: req.cookies["userID"] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const id = req.params.id;
+  
+  if(!req.cookies["userID"]) {
+    res.redirect("/login");
+    return;
+  }
+
+  if(!urlDatabase[id]) {
+    res.send("URL does not exist")
+    return;
+  }
+
+  if(urlDatabase[id].userID !== req.cookies["userID"]) {
+    res.send("That URL does not belong to you");
+    return;
+  }
+
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, users: users, user: req.cookies["userID"] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id].longURL;
+
+  if(!longURL) {
+    res.send("The tinyapp id you're lookin for does not exist");
+    return;
+  }
+  res.redirect(longURL);
+})
+
+app.get("/login", (req, res) => {
+  const userID = req.cookies["userID"]
+  if(userID) {
+    res.redirect("/urls");
+    return;
+  }
+  const templateVars = { urls: urlDatabase, users: users, user: userID };
+  res.render("login", templateVars);
+})
+
+app.get("/register", (req, res) => {
+  if(req.cookies["userID"]) {
+    res.redirect("/urls");
+    return;
+  }
+  const templateVars = { urls: urlDatabase, users: users, user: req.cookies["userID"] };
+  res.render("urls_register", templateVars);
+});
+
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURLName;
+  const id = req.params.id;
+  const userID = req.cookies["userID"];
+
+  if(!urlDatabase[id]) {
+    res.send("That URL id does not exist");
+    return;
+  }
+  if(!userID) {
+    res.send("You are not logged in.");
+    return;
+  }
+  if(urlDatabase[id].userID !== userID) {
+    res.send("You do not own this URL.");
+    return;
+  }
+
+  urlDatabase[req.params.id].longURL = req.body.newURLName;
   res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  const userID = req.cookies["userID"];
+
+  if(!urlDatabase[id]) {
+    res.send("That URL id does not exist");
+    return;
+  }
+  if(!userID) {
+    res.send("You are not logged in.");
+    return;
+  }
+  if(urlDatabase[id].userID !== userID) {
+    res.send("You do not own this URL.");
+    return;
+  }
+
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
+  if(!req.cookies["userID"]) {
+    res.send("Error: You cannot POST to shorten URLS because you are not logged in.");
+    return;
+  }
   console.log(req.body); // Log the POST request body to the console
   const randomString = generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
+  urlDatabase[randomString] = {longURL: req.body.longURL, userID: req.cookies["userID"]};
+  console.log(urlDatabase);
   res.redirect(`/urls/${randomString}`); // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/u/:id", (req, res) => {
-  longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-})
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.post("/login", (req, res) => {
-  res.cookie('username',req.body.username);
-  res.redirect("/urls");
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  for(let id in users) {
+    if(users[id].email === email) {
+      if(users[id].password !== password) {
+        res.statusCode = 403;
+        return res.send("Error: 403, invalid login information");
+      }
+      res.cookie('userID', users[id].ID);
+      res.redirect('/urls');
+      return;
+    }
+  }
+  res.statusCode = 403;
+  res.send("Error: 403, invalid login information");
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("userID");
+  res.redirect("/urls");
+})
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if(email === "" || password === "") {
+    res.statusCode = 400;
+    return res.send("Error 400, email or password fields are empty");
+  }
+  for(let id in users) {
+    if(users[id].email === email) {
+      res.statusCode = 400;
+      return res.send("Error 400, email already exists in our database");
+    }
+  }
+  const ID = generateRandomString();
+  users[ID] = { ID, email, password };
+  res.cookie('userID', ID);
+  console.log(users);
   res.redirect("/urls");
 })
 
